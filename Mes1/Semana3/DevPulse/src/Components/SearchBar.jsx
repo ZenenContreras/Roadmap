94ba713e0105e71ea3ae52a9ad8fafe3efc33624
Zenen Contreras Royero <zenencontreras1@gmail.com>
2026-09-02 16:38:53 -0500

    devpulse: enhance SearchBar styling for better focus and button text visibility

--- Mes1/Semana3/DevPulse/src/Components/SearchBar.jsx
+++ Mes1/Semana3/DevPulse/src/Components/SearchBar.jsx
@@ -7,9 +7,9 @@ function SearchBar({handleSearch, search, setSearch, isLoading}) {
 
   return (
     <form onSubmit={handleSearch} className='flex gap-2 w-full'>
-        <input value={search} onChange={(e) => setSearch(e.target.value)} className='rounded-lg  w-full py-3 px-5 border border-border bg-input' type="text" placeholder='Search Github user...'/>
+        <input value={search} onChange={(e) => setSearch(e.target.value)} className='rounded-lg  w-full py-3 px-5 border border-border bg-input focus:border-input outline-none' type="text" placeholder='Search Github user...'/>
 
-        <button type='submit' disabled={isLoading || !search.trim()} className='border bg-card border-border py-1 px-4 rounded-lg hover:cursor-pointer text-text-secondary'>
+        <button type='submit' disabled={isLoading || !search.trim()} className='border bg-card border-border py-1 px-4 rounded-lg hover:cursor-pointer text-text'>
             {isLoading ? <div className='flex items-center gap-2'>
               <div className="h-4 w-4 animate-spin rounded-full border-4 border-solid border-border border-t-transparent"></div>
               Searching
