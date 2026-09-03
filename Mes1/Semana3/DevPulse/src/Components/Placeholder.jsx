e028ecf889990b7c648c78da13f88dde996d73fe
Zenen Contreras Royero <zenencontreras1@gmail.com>
2026-09-02 22:35:25 -0500

    devpulse: remove unused SVG file, update success color in CSS, and modify Placeholder component for improved icon rendering

--- Mes1/Semana3/DevPulse/src/Components/Placeholder.jsx
+++ Mes1/Semana3/DevPulse/src/Components/Placeholder.jsx
@@ -5,23 +5,23 @@ function Placeholder() {
   return (
     <div className='h-full flex-1 w-full flex flex-col items-center justify-center gap-20'>
         <div className='flex flex-col items-center gap-3'>
-            <img src="github-svgrepo-com (1).svg" alt="" width={100} />
+            <img src="github.svg" alt="" width={100} />
             <h2 className='text-4xl font-bold'>Search For a github developer</h2>
             <p className='text-text-secondary'>Enter a username to explore their profile, repositories and more</p>
         </div>
         <div className='flex items-center justify-between gap-10'>
             <div className='flex flex-col gap-2 m-15'>
-                <User size={40} />
+                <User size={40} strokeWidth={1}/>
                 <h3 className='text-lg text-text'>View Profile</h3>
                 <p className='text-muted'>See their details <br/>and bio </p>
             </div>
             <div className='flex flex-col gap-2 px-15 border-x border-border'>
-                <BookMarked size={40}/>
+                <BookMarked size={40} strokeWidth={1}/>
                 <h3 className='text-lg text-text'>Explore Repositories</h3>
                 <p className='text-muted'>Check their public <br/>repo</p>
             </div>
             <div className='flex flex-col gap-2 m-15'>
-                <Star size={40}/>
+                <Star size={40} strokeWidth={1}/>
                 <h3 className='text-lg text-text'>Discover technologies</h3>
                 <p className='text-muted'>See what they  <br/>work with </p>
             </div>
