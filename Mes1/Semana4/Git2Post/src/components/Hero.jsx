d6a57143a96ba8557ee53b39fc44d9b40a74e3ef
Zenen Contreras Royero <zenencontreras1@gmail.com>
2026-09-06 18:14:42 -0500

    git2post: initial config for the project. folder structure

---
+++ Mes1/Semana4/Git2Post/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section>
+        <h1>Turn your GitHub activity into content.</h1>
+        <p>Transform your commits, repositories and development activity into professional content </p>
+        <button>Connect Github</button>
+    </section>
+  )
+}
+
+export default Hero
