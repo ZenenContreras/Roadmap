afeacef95fd1cb7f73c29bc1b35c73c697fede7f
Zenen Contreras <zenencontreras1@gmail.com>
2026-09-07 11:21:52 -0500

    git2post: implement global state management using useReducer for user context

--- Mes1/Semana4/Git2Post/src/context/Git2PostContext.jsx
+++ Mes1/Semana4/Git2Post/src/context/Git2PostContext.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { createContext, useState } from 'react'
 
 export const UserContext = createContext()
 
-function Git2PostContext({children}) {
+const initialState = {
+  user: null,
+  repositories: [],
+  selectedRepository: null,
+  generatedPost: null,
+}
 
-  const [user, setUser] = useState(null)
+function git2postReducer(state, action) {
+  switch (action.type) {
+    case 'SET_USER': 
+      return {...state, user: action.payload}
+    case 'SET_REPOSITORIES': {
+      return {...state, repositories: action.payload}
+    }
+    case 'SELECT_REPOSITORY': {
+      return {...state, repository: action.payload}
+    }
+    case 'SET_GENERATED_POST': {
+      return {...state, post: action.payload}
+    }
+    case 'RESET': {
+      return initialState
+    }
+    
+
+  }
+}
+
+function Git2PostContext({children}) {
+  const [state, dispatch] = useReducer(git2postReducer, initialState)
 
   return (
-    <UserContext.Provider value={{user, setUser}} >
+    <UserContext.Provider value={{state, dispatch}} >
         {children}
     </UserContext.Provider>
   )
