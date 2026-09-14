a54f4b9b138ad419de6191e9c415dc5ebd128676
Zenen Contreras <zenencontreras1@gmail.com>
2026-09-14 16:19:20 -0500

    git2post: enhance Sidebar component by integrating useToggle hook for state management; add toggle button for improved interactivity

--- Mes1/Semana4/Git2Post/src/components/dashboard/Sidebar.jsx
+++ Mes1/Semana4/Git2Post/src/components/dashboard/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { NavLink, useNavigate } from 'react-router'
 import UseGit2Post from '../../hooks/UseGit2Post'
+import useToggle from '../../hooks/useToggle'
 
 function Sidebar() {
   const navigate = useNavigate()
-  const {state, dispatch} = UseGit2Post()
+  const { dispatch} = UseGit2Post()
+  const {value, toggle} = useToggle()
 
   function handleLogOut(){
     dispatch({type: 'RESET' })
@@ -25,6 +27,8 @@ function Sidebar() {
         <NavLink to='/dashboard/settings' className={({ isActive}) => `${isActive ? "font-bold" : ""} `}>Settings</NavLink>
 
         <button className='bg-amber-50 rounded-lg py-1 px-2 text-black cursor-pointer font-bold' onClick={handleLogOut}>Log Out</button>
+
+        <button onClick={toggle} > {value ? 'TRUE' : 'FALSE'} </button>
       </div>
     </nav>
   )
